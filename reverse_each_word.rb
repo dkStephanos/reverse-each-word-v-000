65621240277a2a68a7846cc53572642a6be65adb
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-27 21:20:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   reversed_words = []
   words = sentence.split(" ")
   words.map do |word|
-    reversed_words.push(word.reversed)
+    reversed_words.push(word.reverse)
   end
   reversed_words
 end
